a4265f75e31e2ce4500aa9137490f343e5bca424
kevinyun31 <kevinyun31@gmail.com>
2023-08-23 14:03:26 +0900

    사진 외부경로 설정하기

--- src/main/java/shop/mtcoding/blogv2/_core/config/WebMvcConfig.java
+++ src/main/java/shop/mtcoding/blogv2/_core/config/WebMvcConfig.java
@@ -1,14 +1,24 @@
 package shop.mtcoding.blogv2._core.config;
 
-import org.apache.tomcat.util.file.ConfigurationSource.Resource;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 // 사진 외부경로 표시하는 법
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations("file:"+"./images/")
+            .setCachePeriod(10) // 10 (초)
+            .resourceChain(true)
+            .addResolver(new PathResourceResolver());
+    }
 
 
     }
